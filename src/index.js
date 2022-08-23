14ca7db0c586235e554143aa5a90f9b0fcc08e86
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-08-23 16:31:38 +0300

    Add and Solve Linters Errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
-import Store from './modules/store';
-import UI from './modules/ui';
-import User from './modules/user';
+import Store from './modules/store.js';
+import UI from './modules/ui.js';
+import User from './modules/user.js';
 
 // display users onload of the page
 document.addEventListener('DOMContentLoaded', UI.displayUsers);
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,4 +1,4 @@
-import Store from './store';
+import Store from './store.js';
 
 class UI {
   static displayUsers() {
