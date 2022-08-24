465b5b871738a4ac8d5b44bbc2116b220b43f4ae
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-08-24 13:09:14 +0300

    Api connected

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,18 +1,47 @@
 class Store {
-  static getUser() {
-    let users;
-    if (localStorage.getItem('users') === null) {
-      users = [];
-    } else {
-      users = JSON.parse(localStorage.getItem('users'));
-    }
-    return users;
+  // static getUser() {
+  //   let users;
+  //   if (localStorage.getItem('users') === null) {
+  //     users = [];
+  //   } else {
+  //     users = JSON.parse(localStorage.getItem('users'));
+  //   }
+  //   return users;
+  // }
+
+  // static addUser(user) {
+  //   const users = this.getUser();
+  //   users.push(user);
+  //   localStorage.setItem('users', JSON.stringify(users));
+  // }
+
+  static sendUser(user) {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+  }
+  
+  static sendNewScore(user) {
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/nONLcYfe5VIS7yt3lQIO/scores/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
   }
 
-  static addUser(user) {
-    const users = this.getUser();
-    users.push(user);
-    localStorage.setItem('users', JSON.stringify(users));
+  static getScores() {
+    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/nONLcYfe5VIS7yt3lQIO/scores/';
+    const data = await fetch(url);
+    const scores = await data.json();
+    return scores.result;
   }
 }
 
