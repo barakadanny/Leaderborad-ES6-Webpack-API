081ea72514ef3a1d1b1e341f4fc709d30e08a2df
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-08-25 13:10:27 +0300

    Refactor store.js sendNewScore function

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,26 +1,15 @@
-const userName = document.querySelector('#name').value;
-const userScore = document.querySelector('#score').value;
+// const userName = document.querySelector('#name').value;
+// const userScore = document.querySelector('#score').value;
 
-const sendNewScore = () => {
-  fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fadf/scores',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({ user: userName, score: userScore }),
-    }
-  );
+const sendNewScore = async () => {
+  const post = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  await fetch(url, post);
 };
 
-const getScores = async () => {
-  const url =
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fadf/scores';
-  const data = await fetch(url);
-  const scores = await data.json();
-  return scores.result;
-};
-
-export { sendNewScore, getScores };
+export default sendNewScore;
